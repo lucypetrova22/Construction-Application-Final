a1670b5a69075996d7f4853e69e8dd195a7b106a
Lucy Petrova <164482879+lucypetrova22@users.noreply.github.com>
2025-05-19 01:05:36 +0300

    Update dbcon.java

--- src/dbcon.java
+++ src/dbcon.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class dbcon {
     private static final String URL = "jdbc:mysql://localhost:3306/ConstructionDB";
     private static final String USER = "root";
-    private static final String PASSWORD = "Tiger064310.";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() {
         try {
